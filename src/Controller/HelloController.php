c54c4473a2c67e227f55cdec25bb204e7dbc0805
caucroge <caucroge.dev@gmail.com>
2021-05-10 17:55:08 +0200

    Create Twig : hello - Update Controller : Hello

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,34 +2,18 @@
 
 namespace App\Controller;
 
-use App\Taxes\Detector;
-use App\Taxes\Calculator;
-use Cocur\Slugify\Slugify;
-use Psr\Log\LoggerInterface;
+use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
-    protected $calculator;
-
-    public function __construct(Calculator $calculator)
-    {
-        $this->calculator = $calculator;
-    }
-
     #[Route("/hello/{prenom?le monde !}", name: "hello")]
-    public function hello($prenom, LoggerInterface $logger, Slugify $slugify, Detector $detector)
+    public function hello($prenom, Environment $twig)
     {
-        dump($detector->detect(101));
-        dump($detector->detect(9));
-
-        dump($slugify->slugify("Bonjour le monde"));
-
-        $tva = $this->calculator->calcul(100);
-        dump($tva);
-
-        $logger->error("Mon message de log");
-        return new Response("hello $prenom");
+        $html = $twig->render('hello.html.twig', [
+            'prenom' => $prenom
+        ]);
+        return new Response($html);
     }
 }

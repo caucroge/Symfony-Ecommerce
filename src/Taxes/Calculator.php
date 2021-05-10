6546a1cbcbd687a2b35492fc46d59cab12d4be91
caucroge <caucroge.dev@gmail.com>
2021-05-10 12:01:38 +0200

    Update Classe : Calculator

--- src/Taxes/Calculator.php
+++ src/Taxes/Calculator.php
@@ -2,10 +2,20 @@
 
 namespace App\Taxes;
 
+use Psr\Log\LoggerInterface;
+
 class Calculator
 {
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function calcul(float $prix) : float
     {
+        $this->logger->info("Utilisation du service Calculator");
         return $prix * (20/100);
     }
 }

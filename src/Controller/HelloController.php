f7ebd032ff5e427d829878907c9f7129631ba84f
caucroge <caucroge.dev@gmail.com>
2021-05-10 12:33:47 +0200

    Add composer : cocur\slugify - Update Controller : Hello - Update Config : services

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +18,9 @@ class HelloController
     }
 
     #[Route("/hello/{prenom?le monde !}", name: "hello")]
-    public function hello(LoggerInterface $logger, $prenom)
+    public function hello($prenom, LoggerInterface $logger, Slugify $slugify)
     {
+        dump($slugify->slugify("Bonjour le monde"));
         $tva = $this->calculator->calcul(100);
         dump($tva);
 

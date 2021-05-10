5284b18ec76ee307f93bb1f27f96c13e93836920
caucroge <caucroge.dev@gmail.com>
2021-05-10 11:28:25 +0200

    Update Controller : Hello

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
     #[Route("/hello/{prenom?le monde !}", name: "hello")]
-    public function hello($prenom)
+    public function hello(LoggerInterface $logger, $prenom)
     {
+        $logger->error("Mon message de log");
         return new Response("hello $prenom");
     }
 }

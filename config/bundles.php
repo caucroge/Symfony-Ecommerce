730a256dce8949167a3de4537c7a110f1de7014c
caucroge <caucroge.dev@gmail.com>
2021-05-10 22:36:16 +0200

    Update Twig : base & hello

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];

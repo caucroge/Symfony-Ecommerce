0d7fd74398c99f4662b8b16f47a0049445e39a7b
caucroge <caucroge.dev@gmail.com>
2021-05-12 00:09:52 +0200

    Update Controller : HomePage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomePageController extends AbstractController
 {
     #[Route("/", name: "homepageIndex")]
-    public function index()
+    public function index(ProductRepository $productRepository)
     {
+        $product = $productRepository->find(1);
+        dump($product);
         return $this->render('homepage/index.html.twig');
     }
 }

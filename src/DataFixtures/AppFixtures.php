3563a0b18d69e252267557b20d3ddf700c76aa68
caucroge <caucroge.dev@gmail.com>
2021-05-12 22:06:19 +0200

    Add Composer : mbezhanov/faker-provider-collection & liorchamla/faker-prices - Update Fixtures : AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Product;
+use Liior\Faker\Prices;
+use Bezhanov\Faker\Provider\Commerce;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -12,12 +14,14 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+        $faker->addProvider(new Prices($faker));
+        $faker->addProvider(new Commerce($faker));
 
         for($i = 0; $i < 100; $i++)
         {
             $product = new Product;
-            $product->setName($faker->sentence())
-                    ->setPrice(mt_rand(100,200))
+            $product->setName($faker->productName())
+                    ->setPrice($faker->price(4000, 20000))
                     ->setSlug($faker->slug());
             $manager->persist($product);
         }

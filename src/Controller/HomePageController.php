1be3b3f2bf03bddd956f993f0be9e34c12138c9e
caucroge <caucroge.dev@gmail.com>
2021-05-12 14:04:53 +0200

    Update Controller : HomePage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomePageController extends AbstractController
 {
     #[Route("/", name: "homepageIndex")]
-    public function index(ProductRepository $productRepository)
+    public function index(EntityManagerInterface $em)
     {
-        $product = $productRepository->find(1);
-        dump($product);
+        // $product = new Product;
+        // $product->setName("Chaise en plasique")
+        //         ->setPrice(1500)
+        //         ->setSlug("Chaise-en-plastique");
+        // $em->persist($product);
+
+        $productRepository = $em->getRepository(Product::class);
+        $product = $productRepository->find(3);
+        // $product->setPrice(2500);
+        // $em->flush();
+
+        $em->remove($product);
+        $em->flush();
+
         return $this->render('homepage/index.html.twig');
     }
 }

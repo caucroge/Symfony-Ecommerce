667cffd34bd824fd1b5655868f46ce263cac3f72
caucroge <caucroge.dev@gmail.com>
2021-05-18 13:59:54 +0200

    (11.04) - Transformer des donnes avec un DataTransformer

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -7,13 +7,12 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 
 class ProductType extends AbstractType
 {
@@ -66,27 +65,20 @@ class ProductType extends AbstractType
                 ]
             );
 
-        $builder->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $formEvent) {
-                /**@var Product */
-                $product = $formEvent->getData();
-                if ($product->getPrice() !== null) {
-                    $product->setPrice($product->getPrice() * 100);
+        $builder->get('price')->addModelTransformer(new CallbackTransformer(
+            function ($value) {
+                if ($value === null) {
+                    return;
                 }
-            }
-        );
-
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $formEvent) {
-                /**@var Product */
-                $product = $formEvent->getData();
-                if ($product->getPrice() !== null) {
-                    $product->setPrice($product->getPrice() / 100);
+                return $value / 100;
+            },
+            function ($value) {
+                if ($value === null) {
+                    return;
                 }
+                return $value * 100;
             }
-        );
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver)

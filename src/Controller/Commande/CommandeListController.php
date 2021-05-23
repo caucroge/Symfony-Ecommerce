b69c52fb8a57b985cda6d30a99e224a3fcb564b4
caucroge <caucroge.dev@gmail.com>
2021-05-23 17:53:32 +0200

    (17.06) - Update Controller : CommandeList - Update Type : Price

--- src/Controller/Commande/CommandeListController.php
+++ src/Controller/Commande/CommandeListController.php
@@ -4,19 +4,25 @@ namespace App\Controller\Commande;
 
 use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class CommandeListController extends AbstractController
 {
     #[Route('/commande', name: 'commande_index')]
+    // /**
+    //  * @Security("is_granted('ROLE_USER')", message="Vous devez être connecter pour accéder à vos commandes")
+    //  */
+    /**
+     * @IsGranted("ROLE_USER")
+     */
     public function index()
     {
         /** @var User */
         $user = $this->getUser();
-
-        if (!$user) {
-            $this->redirectToRoute('index');
-        }
 
         return $this->render('commande/index.html.twig', [
             'commandes' => $user->getCommandes(),
